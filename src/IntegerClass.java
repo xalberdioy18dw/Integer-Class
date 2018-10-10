4de1c77d125eca2b38810e5d7e766e1737c96c60
ik013043z1 <ik013043z1@H013043.NET>
2018-10-10 09:56:52 +0200

    first commit

---
+++ src/IntegerClass.java
@@ -0,0 +1,23 @@
+public class IntegerClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer a = 2;
+		Integer b = 3;
+		System.out.println(a.compareTo(b));
+		
+		Integer y = 15;
+		 
+	      int i = y.intValue();
+	      System.out.println("Value of i = " + i);
+	      
+	      Integer obj = 100;
+	      short s = obj.shortValue();
+	      System.out.println("Value of s is: " + s);
+	      
+	      Integer x = 5;
+	      System.out.println(x.toString());  
+	      System.out.println(Integer.toString(12)); 
+	}
+
+}
