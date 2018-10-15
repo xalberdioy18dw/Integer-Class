a244995d299e448e0801dd3e47bf51a6b92d866f
ik013043z1 <ik013043z1@H013043.NET>
2018-10-15 08:31:30 +0200

    first commit

--- src/IntegerClass.java
+++ src/IntegerClass.java
@@ -6,18 +6,37 @@ public class IntegerClass {
 		Integer b = 3;
 		System.out.println(a.compareTo(b));
 		
+		String str = new String("Xabier Alberdi");
+		System.out.println(Integer.getInteger(str, 6));
+		System.out.println(Integer.getInteger("abcd",5));
+				
+		
 		Integer y = 15;
-		 
-	      int i = y.intValue();
-	      System.out.println("Value of i = " + i);
+	    int i = y.intValue();
+	    System.out.println("Value of i = " + i);
 	      
-	      Integer obj = 100;
-	      short s = obj.shortValue();
-	      System.out.println("Value of s is: " + s);
+	    String str2 = new String("123");
+	    int num1 = 100;
+	    int num2 = Integer.parseInt(str2);
+	    int sum = num1 + num2;
+	    System.out.println("The value of the sum is:" + sum);
 	      
-	      Integer x = 5;
-	      System.out.println(x.toString());  
-	      System.out.println(Integer.toString(12)); 
+	      
+	    Integer obj = 100;
+	    short s = obj.shortValue();
+	    System.out.println("Value of s is: " + s);
+	      
+	    Integer x = 5;
+	    System.out.println(x.toString());  
+	    System.out.println(Integer.toString(12)); 
+	    
+	    String length = "10";
+		String width = "12";
+		// convert string input into byte
+		byte length2 = Byte.valueOf(length);
+		byte width2 = Byte.valueOf(width);
+		byte area = (byte)(length2 * width2);
+		System.out.print(area);
 	}
 
 }
