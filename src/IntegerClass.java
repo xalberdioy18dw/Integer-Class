dac7b31430e19d7e3d54f6c0416903da05cc321b
ik013043z1 <ik013043z1@H013043.NET>
2018-10-15 08:33:18 +0200

    first commit

--- src/IntegerClass.java
+++ src/IntegerClass.java
@@ -5,30 +5,35 @@ public class IntegerClass {
 		Integer a = 2;
 		Integer b = 3;
 		System.out.println(a.compareTo(b));
+		// It compares both numbers and it prints what difference is between them.
 		
 		String str = new String("Xabier Alberdi");
 		System.out.println(Integer.getInteger(str, 6));
 		System.out.println(Integer.getInteger("abcd",5));
-				
+		// It determines the integer value of the system property with the specified name.	
 		
 		Integer y = 15;
 	    int i = y.intValue();
 	    System.out.println("Value of i = " + i);
+	    // It returns the value as an int.
 	      
 	    String str2 = new String("123");
 	    int num1 = 100;
 	    int num2 = Integer.parseInt(str2);
 	    int sum = num1 + num2;
 	    System.out.println("The value of the sum is:" + sum);
+	    // It is used to get the primitive data type of a certain String. 
 	      
 	      
 	    Integer obj = 100;
 	    short s = obj.shortValue();
 	    System.out.println("Value of s is: " + s);
+	    // It returns the value as an int.
 	      
 	    Integer x = 5;
 	    System.out.println(x.toString());  
 	    System.out.println(Integer.toString(12)); 
+	    // It converts an integer into a string. 
 	    
 	    String length = "10";
 		String width = "12";
@@ -37,6 +42,7 @@ public class IntegerClass {
 		byte width2 = Byte.valueOf(width);
 		byte area = (byte)(length2 * width2);
 		System.out.print(area);
+		// It returns a Byte object holding the value represented by the string argument.
 	}
 
 }
